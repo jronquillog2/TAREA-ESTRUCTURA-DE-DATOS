e20e18cec06f4b46743f2e20e40db78c3b4a2865
Juan Ronquillo <85717668+jronquillog2@users.noreply.github.com>
2021-07-02 16:50:08 -0500

    Add files via upload

--- ejercicio8.py
+++ ejercicio8.py
@@ -5,7 +5,7 @@ print("Consulta si es aceptado o recazado ")
 C1 = float(input("ingrese la calificacion del  primer examen:"))
 C2 = float(input("ingrese la calificacion del segundo examen:"))
 notafinal = C1+C2
-if notafinal >= 80:
+if (c1 >= 80) and (c2 >= 80):
     print ("ACEPTADO")
 else:
     print("RECHAZAD0")
